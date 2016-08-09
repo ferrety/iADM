844cca3917664b6b880dd50a6f9cb70538b8aad2
Vesa Ojalehto <vesa.ojalehto@finnopt.com>
2016-08-09 13:36:23 +0300

    Submitted to PPSN2016 2016-04-15

--- pyMOEA.py
+++ pyMOEA.py
@@ -1,15 +1,26 @@
 import os
 import sys
+import itertools
+import tempfile
+import shutil
+from sklearn import tree
 
-JAVA_HOME=r'C:\Program Files\Java\jdk1.8.0_25'
+try:
+    JAVA_HOME=os.environ['JAVA_HOME']
+except KeyError:
+    JAVA_HOME=r'C:\Program Files\Java\jdk1.8.0_25'
+    os.environ['JAVA_HOME']=JAVA_HOME    
 
-os.environ['JAVA_HOME']=JAVA_HOME
 os.environ['PATH']+=r';%s\bin;%s\jre\bin\server'%tuple([JAVA_HOME]*2)
 
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from random import random
 
+from scipy import spatial
+from scipy.optimize import differential_evolution
+import numpy as np
 
 try:
     import jnius_config
@@ -30,10 +41,12 @@ except ValueError,e:
 from jnius import autoclass,cast, JavaException
 
 def solve(problem_def,method="RNSGAII",refpoint=None,epsilon=0.00001,evals=10000):
-    MOEA_dir=r"C:\MyTemp\nimbus\hnsga2-moea\MOEAFramework-2.3"
-
+    tmp_dir=tempfile.mkdtemp()
+    olddir=os.getcwd()
+    os.chdir(tmp_dir)
+    
     if refpoint is not None:
-        with open("refpoints.txt","w") as fd:
+        with open(os.path.join("refpoints.txt"),"w") as fd:
             fd.write(str(epsilon)+'\n')
             fd.write(" ".join(map(str,refpoint)))
             fd.close()
@@ -41,7 +54,8 @@ def solve(problem_def,method="RNSGAII",refpoint=None,epsilon=0.00001,evals=10000
     pyMOEARunnner=autoclass('pyMOEARunnner')
     rn=pyMOEARunnner()
     res=rn.solve(method,evals,*problem_def)
-    
+    os.chdir(olddir)
+    shutil.rmtree(tmp_dir)
     points=[]
     siter=res.iterator()
     while siter.hasNext():
@@ -64,17 +78,24 @@ def plot(points):
     plt.show(block=False)
 
 
-def problem(problem_name,nf):
+def problem(problem_name,nf,nx=None):
     try:
-        prob_def=problem_def(problem_name,nf)
+        prob_def=problem_def(problem_name,nf,nx=nx)
     except:
-        raise Exception("Could not generate probelm %s"%problem_name)
+        raise Exception("Could not generate problem %s"%problem_name)
     problemClass=autoclass(prob_def[0])
-    return problemClass(prob_def[1],prob_def[2]) 
+    # Problem families where number of objectives can be changed
+    NF=['DTLZ']
+    for family in NF:
+        if problem_name.startswith(family):
+            return problemClass(prob_def[1],prob_def[2]) 
+    return problemClass(nx) 
 
 def problem_def(problem_name,nf,nx=None):
 
-    NX={'DTLZ':[4]+[9]*3+[None]*2+[19]}
+    NX={'DTLZ':[4]+[9]*3+[None]*2+[19],
+        'ZDT' :[30,30,30,30],
+        }
 
     cdir = None
     for key in NX.keys():
@@ -91,6 +112,7 @@ def problem_def(problem_name,nf,nx=None):
         return None
 
     return (ProblemClassName,nx,nf)
+
 def bounds(problem):
     EPS=0.000001
     bounds=[]
@@ -99,6 +121,111 @@ def bounds(problem):
         var = cast('org.moeaframework.core.variable.RealVariable', sol.getVariable(i))
         bounds.append((var.getLowerBound()+EPS,var.getUpperBound()-EPS))
     return bounds
+
+def iterate(preference,ref,objs,tol=0.01,p=.9):
+    aspir=preference[0]
+    w=preference[1]
+    nf=len(aspir)
+    nadir=[1.0]*nf
+    new_ref=[0.0]*nf
+    #create the decision tree
+    A=np.array(objs)
+    nf = len(ref)
+    trees=[]
+    for i in range(nf):
+        trees.append(tree.DecisionTreeClassifier())
+        trees[-1].fit(A[:,range(0,i)+range(i+1,nf)], A[:,i])
+    
+    obj=objs[-1]
+    S=[None]*len(obj)
+    pk=p
+    for k in reversed(np.argsort(w)):
+        fk=obj[k]
+        if abs(aspir[k]-fk) < tol and random()<p:
+            S[k]=fk
+        elif random()<pk:
+            S[k]=fk
+        if len(S):
+            pk-=pk/len(obj)*len(S)
+    if len(S)==k:
+        return ref
+            
+    for k,s in enumerate(S):
+        if s is not None:
+            new_ref[k]=min(aspir[k]-(aspir[k]-obj[k])/2.,nadir[k])
+        else:
+            new_ref[k]=None
+
+    idx = [idx for idx,v in enumerate(new_ref) if v==None]
+    for i in idx:
+        n=[0.0 if v is None else v for v in new_ref]
+        del n[i]
+        new_ref[i]=min(trees[i].predict(n)[0],nadir[i])
+    if np.array_equal(nadir,new_ref):
+        return ref
+    return new_ref
+
+def ACH(x,problem,ref,rho=0.001):
+    f=evaluate(problem,x)
+    nadir=[1.0]*len(ref)
+    utopian=[0.0]*len(ref)
+    A=np.array([f,ref,nadir,utopian])    
+    return np.max(np.apply_along_axis(lambda x:(x[0]-x[1])/(x[2]-x[3]),0,A)) \
+                 +rho*np.sum(np.apply_along_axis(lambda x:x[0]/(x[2]-x[3]),0,A))
+               
+
+
+def ACH_solution(problem_name,refpoint,evals=10000,nx=None):
+    nf=len(refpoint)
+    prob=problem(problem_name,nf,nx=nx)
+    bds=bounds(prob)
+    res=differential_evolution(ACH,bds,args=(prob,refpoint),maxiter=evals)
+    return evaluate(prob,res.x)
+
+def rNSGAII_solution(problem,refpoint,evals=10000):
+    nf=len(refpoint)
+    points=solve(problem_def(problem,nf),refpoint=refpoint,evals=evals)
+    A=np.array(points)
+    return list(A[spatial.KDTree(A).query(refpoint)[1]])
+
+def agent_run(pref,evals=20000,
+              methods=[ACH_solution,rNSGAII_solution],
+              problems=['DTLZ1','DTLZ2','DTLZ3','DTLZ4']):
+    
+    return Parallel(n_jobs=2)(
+       delayed(agent_solve_problems)(target,pref,evals) 
+            for target in itertools.product(methods,problems)         
+    )
+
+def proj_ref(nf,problem,ref,evals=20000,nx=None):
+    print "Projecting %s_%i for %s"%(problem,nf,str(ref))
+    res=ACH_solution(problem,ref,evals=evals,nx=nx)
+    return (nf,problem,ref,res)
+
+    
+def agent_paraller(target,preference,evals=20000):
+    method=target[0]
+    problem=target[1]
+
+    return agent_solve(method,problem,preference,evals)
+
+def agent_solve(method,problem,preference,evals=20000):
+    print "Solving: %s:%s"%(str((method.__name__,problem)),str(len(preference[0])))
+    w=tuple(preference[1])
+    ref=preference[0][:]
+    nf = len(ref)
+    i=0
+    objs=[]
+    refs=[ref[:]]
+    while ref is not None and i<10:
+        objs.append(method(problem,ref,evals=evals))
+        new_ref=iterate(preference,refs[-1],objs)
+        if np.array_equal(new_ref,refs[-1]):
+            break   
+        refs.append(new_ref[:])
+        i+=1
+    print "Solved: %s:\n%s"%(str((method.__name__,problem)),str(objs[-1]))
+    return objs[-1],preference,(method,problem),(objs,refs)
     
 if __name__=='__main__':
     import os
