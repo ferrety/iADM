128e8eb87339b52e8461e13147b0d0293f70a678
Vesa Ojalehto <vesa.ojalehto@jyu.fi>
2016-11-04 15:56:45 +0200

    ADM Variant 2 and some fixes to Variant 1

--- pyMOEA.py
+++ pyMOEA.py
@@ -4,12 +4,13 @@ import itertools
 import tempfile
 import shutil
 from sklearn import tree
-
+from scipy.spatial import Rectangle
+import copy
 try:
     JAVA_HOME=os.environ['JAVA_HOME']
 except KeyError:
-    JAVA_HOME=r'C:\Program Files\Java\jdk1.8.0_25'
-    os.environ['JAVA_HOME']=JAVA_HOME    
+    JAVA_HOME=r'C:\Program Files\Java\jdk1.8.0_31'
+    os.environ['JAVA_HOME']=JAVA_HOME
 
 os.environ['PATH']+=r';%s\bin;%s\jre\bin\server'%tuple([JAVA_HOME]*2)
 
@@ -29,7 +30,16 @@ try:
     for d in ['bin',r'lib\*',r'build\MOEAFramework-2.3\lib']:
         jnius_config.add_classpath(os.path.join(moea_path,d))
 
-    moea_clspath=r"C:\MyTemp\nimbus\hnsga2-moea\MOEAFramework-2.3\bin;C:\MyTemp\nimbus\hnsga2-moea\MOEAFramework-2.3\lib\commons-cli-1.2.jar;C:\MyTemp\nimbus\hnsga2-moea\MOEAFramework-2.3\lib\commons-codec-1.8.jar;C:\MyTemp\nimbus\hnsga2-moea\MOEAFramework-2.3\lib\commons-lang3-3.1.jar;C:\MyTemp\nimbus\hnsga2-moea\MOEAFramework-2.3\lib\commons-math3-3.1.1.jar;C:\MyTemp\nimbus\hnsga2-moea\MOEAFramework-2.3\lib\jcommon-1.0.20.jar;C:\MyTemp\nimbus\hnsga2-moea\MOEAFramework-2.3\lib\jfreechart-1.0.15.jar;C:\MyTemp\nimbus\hnsga2-moea\MOEAFramework-2.3\lib\JMetal-4.3.jar;C:\MyTemp\nimbus\hnsga2-moea\MOEAFramework-2.3\lib\rsyntaxtextarea.jar".split(";")
+    moea_clspath=[
+                 r"C:\MyTemp\nimbus\hnsga2-moea\MOEAFramework-2.3\bin;",
+                  "C:\MyTemp\nimbus\hnsga2-moea\MOEAFramework-2.3\lib\commons-cli-1.2.jar;",
+                  "C:\MyTemp\nimbus\hnsga2-moea\MOEAFramework-2.3\lib\commons-codec-1.8.jar;",
+                  "C:\MyTemp\nimbus\hnsga2-moea\MOEAFramework-2.3\lib\commons-lang3-3.1.jar;",
+                  "C:\MyTemp\nimbus\hnsga2-moea\MOEAFramework-2.3\lib\commons-math3-3.1.1.jar;",
+                  "C:\MyTemp\nimbus\hnsga2-moea\MOEAFramework-2.3\lib\jcommon-1.0.20.jar;",
+                  "C:\MyTemp\nimbus\hnsga2-moea\MOEAFramework-2.3\lib\jfreechart-1.0.15.jar;",
+                  "C:\MyTemp\nimbus\hnsga2-moea\MOEAFramework-2.3\lib\JMetal-4.3.jar;",
+                  "C:\MyTemp\nimbus\hnsga2-moea\MOEAFramework-2.3\lib\rsyntaxtextarea.jar"]
     jnius_config.add_classpath(*moea_clspath)
 
 except ValueError,e:
@@ -39,6 +49,64 @@ except ValueError,e:
     #print(jnius_config.get_classpath())
 
 from jnius import autoclass,cast, JavaException
+
+
+def ADM2_reference(rectangles):
+    """ Generate new reference point for ADM"
+
+    Selects the point with the maximum value of the utility function as the reference point
+
+    """
+    r=1
+    a=.5
+    cec_utility=np.vectorize(lambda x:np.power(np.sum(np.power(x.mins,r)),1./r))
+    return rectangles[np.argmax(cec_utility(rectangles))].mins
+
+
+def ADM2_solve(method,problem, rectangles,evals=2000):
+    print "AMD2 Solving: %s:%s"%(str((method.__name__,problem)),str(len(rectangles)))
+    max_iter=0
+    objs=[]
+    refs=[ADM2_reference(rectangles[-1])]
+    nf = len(rectangles[-1][-1].mins)
+    while max_iter<10:
+        new_rectangles=copy.deepcopy(rectangles[-1])
+        objs.append(method(problem,refs[-1],evals=evals))
+        
+        #print "ref point {} -> {}".format(refs[-1],objs[-1])
+        srnd=lambda v: list(map(lambda x:round(x,6),v))
+        ref_str="%s"%srnd(refs[-1])
+        print format("ref point %s -> %s"%(ref_str.ljust(10*nf),srnd(objs[-1])))
+        #print format("ref point %20s -> %10s"%tuple(map(srnd,[refs[-1],list(objs[-1])])))
+        replace_rec=None
+        for rec in new_rectangles:
+            if rec.min_distance_point(objs[-1])==0.0:
+                replace_rec=rec
+                break
+
+        if replace_rec is None:
+            print("New PO point %s out of bounds\n     For ref %s"%(objs[-1],refs[-1]))
+            break
+        new_rectangles.remove(replace_rec)
+        rectangles.append(new_rectangles)
+        
+        for i,obj in enumerate(objs[-1][:]):
+            low=list(replace_rec.mins)
+            low[i]=obj
+            up=objs[-1][:]
+            up[i]=list(replace_rec.maxes)[i]
+            rectangles[-1].append(Rectangle(low,up))
+            #rectangles[-1].append(Rectangle((replace_rec.maxes[0],replace_rec.mins[1]),objs[-1]))
+
+            
+        ref=ADM2_reference(rectangles[-1])
+        if np.array_equal(ref,refs[-1]):
+            print "Could not generate new refpoint %s"%objs[-1]
+            break
+        refs.append(ref)
+        max_iter +=1
+    return objs[-1],rectangles,(method.__name__,problem),(objs,refs)
+
 
 def solve(problem_def,method="RNSGAII",refpoint=None,epsilon=0.00001,evals=10000):
     tmp_dir=tempfile.mkdtemp()
@@ -188,45 +256,43 @@ def rNSGAII_solution(problem,refpoint,evals=10000):
     A=np.array(points)
     return list(A[spatial.KDTree(A).query(refpoint)[1]])
 
-def agent_run(pref,evals=20000,
-              methods=[ACH_solution,rNSGAII_solution],
-              problems=['DTLZ1','DTLZ2','DTLZ3','DTLZ4']):
-    
-    return Parallel(n_jobs=2)(
-       delayed(agent_solve_problems)(target,pref,evals) 
-            for target in itertools.product(methods,problems)         
-    )
-
 def proj_ref(nf,problem,ref,evals=20000,nx=None):
     print "Projecting %s_%i for %s"%(problem,nf,str(ref))
     res=ACH_solution(problem,ref,evals=evals,nx=nx)
     return (nf,problem,ref,res)
 
-    
-def agent_paraller(target,preference,evals=20000):
+
+def agent_paraller_orig(target,preference,adm=1,evals=20000):
     method=target[0]
     problem=target[1]
 
     return agent_solve(method,problem,preference,evals)
 
-def agent_solve(method,problem,preference,evals=20000):
+
+def agent_paraller(target,preference,adm=1,evals=20000):
+    method=target[0]
+    problem=target[1]
+
+    return ADM2_solve(method,problem,preference,evals)
+
+def agent_solve(method,problem,preference,adm=1,evals=20000):
     print "Solving: %s:%s"%(str((method.__name__,problem)),str(len(preference[0])))
     w=tuple(preference[1])
-    ref=preference[0][:]
-    nf = len(ref)
+
     i=0
     objs=[]
-    refs=[ref[:]]
-    while ref is not None and i<10:
-        objs.append(method(problem,ref,evals=evals))
+    refs=[preference[0][:]]
+    nf = len(refs[0])
+    while refs[-1] is not None and i<10:
+        objs.append(method(problem,refs[-1],evals=evals))
         new_ref=iterate(preference,refs[-1],objs)
         if np.array_equal(new_ref,refs[-1]):
-            break   
+            break
         refs.append(new_ref[:])
         i+=1
     print "Solved: %s:\n%s"%(str((method.__name__,problem)),str(objs[-1]))
     return objs[-1],preference,(method,problem),(objs,refs)
-    
+
 if __name__=='__main__':
     import os
     import sys
